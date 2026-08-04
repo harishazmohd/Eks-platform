d4fc904059be04684a226428ef93a9a9b64fea97
Haris <harisharun592@gmail.com>
2026-08-04 11:37:52 -0700

    feat(rds): add database subnet group

---
+++ florence-infra/modules/database/locals.tf
@@ -0,0 +1,21 @@
+locals {
+  name_prefix = "${var.project_name}-${var.environment}"
+
+  names = {
+    security_group = "${local.name_prefix}-sg"
+    subnet        = "${local.name_prefix}-subnet-group"
+    parameter_group = "${local.name_prefix}-parameter-group"
+    option_group    = "${local.name_prefix}-option-group"
+    database        = "${local.name_prefix}-postgress-database"
+  }
+
+  common_tags = merge(var.common_tags,
+    {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Owner       = var.metadata.owner
+      Repository  = var.metadata.repository
+    }
+  )
+}
---
+++ florence-infra/modules/database/variables.tf
@@ -0,0 +1,78 @@
+variable "project_name" {
+  description = "Name of the project goes here"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment of the project"
+  type        = string
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment can only be: dev, prod or staging"
+  }
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Map of subnets used in this project"
+  type = list(string)
+}
+
+variable "security_group_ids" {
+  description = "Map of security groups used in this project"
+  type        = list(string)
+}
+
+variable "kms_key_arn" {
+  description = "KMS Key for the database"
+  type = string
+}
+
+variable "database_config" {
+  description = "Database configuration"
+  type = object({
+    instance_config = {
+      instance_class = string
+      engine = string
+      engine_version = string
+    }
+    storage_config = {
+      allocated_storage = number
+      max_allocated_storage = optional(number)
+      storage_type      = string
+      storage_encrypted = bool
+      iops              = optional(number)
+    }
+    multiaz = bool
+    backup_retention_period = number
+    deletion_protection      = bool
+    performance_insights_enabled = bool
+    monitoring_interval        = number
+    auto_minor_version_upgrade = bool
+    apply_immediately          = bool
+    port                       = number
+  })
+}
+
+variable "metadata" {
+  description = "Metadata for the AWS resources"
+  type = object({
+    owner      = string
+    repository = string
+  })
+}
+
+variable "common_tags" {
+  description = "Additional tags applied to all resources."
+  type        = map(string)
+  default     = {}
+}
