43aaa6275dd8fe77c8dbf7529063b447f30d9208
Haris <harisharun592@gmail.com>
2026-08-04 10:11:45 -0700

    fix(kms): fixed output issue in kms module

--- florence-infra/modules/kms/outputs.tf
+++ florence-infra/modules/kms/outputs.tf
@@ -1,17 +1,13 @@
 output "kms" {
   description = "AWS KMS resources"
   value = {
-    keys = {
-
       for name, key in aws_kms_key.this :
       name => {
         id          = key.id
         arn         = key.arn
         key_id      = key.key_id
         alias       = aws_kms_alias.this[name].name
-        key_state   = key.key_state
         description = key.description
       }
-    }
   }
 }
