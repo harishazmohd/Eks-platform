72e72a5c2fa096318053d620d7caefa1806cfd76
Haris <harisharun592@gmail.com>
2026-08-29 13:25:25 +0530

    fix: fixed errors related to terraform

--- terraform/modules/eks/manifests.tf
+++ terraform/modules/eks/manifests.tf
@@ -16,5 +16,18 @@ resource "kubectl_manifest" "argocd" {
     server          = "https://kubernetes.default.svc"
     namespace       = "bankapp"
   })
-  depends_on = [helm_release.argocd, kubectl_manifest.appproject]
+  depends_on = [
+    helm_release.argocd,
+    kubectl_manifest.appproject,
+    time_sleep.wait_for_argocd_cleanup
+  ]
 }
+
+resource "time_sleep" "wait_for_argocd_cleanup" {
+  depends_on = [
+    helm_release.aws_load_balancer_controller,
+    helm_release.external_secrets
+  ]
+  destroy_duration = "90s"
+}
+
--- terraform/modules/eks/namespace.tf
+++ terraform/modules/eks/namespace.tf
@@ -2,5 +2,5 @@ resource "kubernetes_namespace_v1" "bankapp" {
   metadata {
     name = "bankapp"
   }
-  depends_on = [ aws_eks_cluster.this, aws_eks_node_group.this ]
+  depends_on = [aws_eks_cluster.this, aws_eks_node_group.this]
 }
